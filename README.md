f7030458625d436432a08bc179a8d4d0a016d813
CatFishing4Guyz <schp4544@gmail.com>
2022-11-14 15:23:31 +0800

    Fixed most things, removed `should_persist`, no longer uses Heroku, you have to find some other way

--- README.md
+++ README.md
@@ -1,57 +1,27 @@
 ## Hungary Time, the Discord bot
-I decided Googling the time in Hungary to see whether the server admin (who is Hungarian)
-was awake or not was too tiring, so I made this abomination.
+I decided Googling the time in Hungary to see whether the server admin (who is Hungarian) was awake
+or not was too tiring, so I made this abomination.
 
-[Invite the bot.](https://discord.com/api/oauth2/authorize?client_id=849610604763283496&permissions=2048&scope=bot%20applications.commands)
+[Invite the bot.](https://discord.com/api/oauth2/authorize?client_id=1041597587297419344&permissions=277025392640&scope=bot%20applications.commands)
 
-I'm not adding a way to tell the actual time with DST in effect, I don't know how to do that.
-If you do, hmu.
+I'm not adding a way to tell the actual time with DST in effect, I don't know how to do that. If
+you do, hmu.
 
-Other than Hungary, I've added other timezones, right now there's:
-
-`Alaska`
-  
-`Mountain`
- 
-`Philippines`
-
-`Central`
-
-`Eastern` 
-
-`Samoa`
-
-`UK`
-
-`Pacific`
-
-`India`
-
-To use, your bot's token, then make a new environment variable named `TOKEN`, and paste your
-bot's token in the value.
-
-*Note: Using `guild_ids` is nice if you want to update the commands immediately, but it also
-might cause a 405 error which I have no idea how to fix. This is harmless but it gets annoying
-and might scare you.*
+Other than Hungary, this bot supports every timezone `pytz` has to offer. Check [this gist](https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568)
+by heyalexej for the list.
 
 ### If you're wondering what the modules are for, here's an explanation
 `py-cord` is a maintained fork of `discord.py`, an API wrapper which was abandoned last year.
-Also be sure to add `--pre` at the end of `pip install py-cord` since the prerelease has the slash 
-commands and other things.
+Also be sure to add `--pre` at the end of `pip install py-cord` since the prerelease has the
+slash commands and other things.
 
 `pytz` provides the timezones and stuff.
 
 `datetime` uses `pytz` to tell the time in those timezones.
 
-Lastly, if you want to host it yourself all the time, you can use
-[this random Medium article](https://medium.com/analytics-vidhya/how-to-host-a-discord-py-bot-on-heroku-and-github-d54a4d62a99e).
-It's a bit outdated but it mostly works. There was also a [security breach](https://status.heroku.com/incidents/2413)
-that happened recently, so attempting to connect your GitHub account won't work (Internal server error).
-In the meantime, just do what [this StackOverflow answer](https://stackoverflow.com/a/71895325) says.
-
 ## Credits
 CatFishing4Guyz, coding the bot, suggestions, testing.
 
-Swemit, testing, and encouragement.
+xChibi Robo, testing, suggestions.
 
 Thank you for the help :)
--- hungarytime.py
+++ hungarytime.py
@@ -1,98 +1,45 @@
-# Made by CatFishing4Guyz#8137 on Discord, gimme praise
+# Made by CatFishing4Guyz#7903 on Discord, gimme praise
 
 import discord
 import pytz
-import asyncio
 import os
 from datetime import datetime as dt
 from discord import Option
 
-REPLACE_WITH_GUILD_ID = os.getenv('GUILD_ID')
-
 bot = discord.Bot(activity = discord.Activity(
-    type=discord.ActivityType.watching, 
-    name="ur mom undress."))
+                                type=discord.ActivityType.watching, 
+                                name="ur mom undress."))
 
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-"""
-Remove the guild IDs and replace it
-with your own server's ID if you want
-slash commands to update immediately.
-
-Enable developer mode in Discord and
-right click on your server from the 
-server list and select "Copy ID".
-
-You might get a 405 error, but that's
-harmless and it won't affect anything.
-"""
-
-@bot.slash_command(guild_ids=[REPLACE_WITH_GUILD_ID], description="Sends the time in Hungary.")
+MY_GUILD_ID = os.getenv('GUILD_ID')
+@bot.slash_command(guild_ids=[MY_GUILD_ID], description="Check the time in various timezones")
 async def time(ctx, timezone: Option(str, required=False, default="Europe/Budapest",
-                                     description="Tell the time where?",
-                                     choices=[
-                                        "Europe/Budapest",
-                                        "Asia/Kolkata",
-                                        "Asia/Manila",
-                                        "Europe/London",
-                                        "US/Alaska",
-                                        "US/Central",
-                                        "US/Eastern",
-                                        "US/Mountain",
-                                        "US/Pacific",
-                                        "US/Samoa",
-                                     ]
-                              ),
-               # Actually `bool`, but that doesn't work
-               should_persist: Option(str, required=True, default="False",
-                                      description="Hourly time update. Choose False if you don't care",
-                                      choices=[
-                                        "False",
-                                        "True",
-                                      ]
-                               )
-          ):
+                                     description="Tell the time where?")):
     DST = " If DST is in effect, the actual time is an hour ahead."
     def Time(Timezone):
-        return dt.now(
-            pytz.timezone(
-                f"{Timezone}")).strftime(
-                    f"It is %d %B %Y, %H:%M in {Timezone}.") + DST
-    if bool(should_persist):
-        # Definitely does not work
-        await ctx.respond("Timekeep initialized.")
-        await asyncio.sleep(1)
-        while True:
-            await ctx.send(Time("Europe/Budapest"))
-            if ctx.content == "test":
-                await ctx.reply("Test")
-                break
-    else:
-        await ctx.respond(Time(timezone)) 
+        return dt.now(pytz.timezone(f"{Timezone}")).strftime(f"It is %d %B %Y, %H:%M in {Timezone}.") + DST
+    await ctx.respond(Time(timezone))
+    
+    # not working
+    try:
+        pass
+    except UnknownTimeZoneError:
+        await ctx.respond("Not a valid timezone. Check out this gist for a list of timezones: \n"
+                          "<https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568>")
 
-@bot.slash_command(guild_ids=[REPLACE_WITH_GUILD_ID], description="Send the link to the GitHub repository.")
+@bot.slash_command(guild_ids=[MY_GUILD_ID], description="Send the link to the GitHub repository.")
 async def github(ctx):
     await ctx.respond("Hi! You can visit my repository here:\n"
                       "https://github.com/CatFishing4Guyz/hungarytime_discordbot")
 
-@bot.slash_command(guild_ids=[REPLACE_WITH_GUILD_ID], description="Sends the list of commands, most are self-explanatory")
+@bot.slash_command(guild_ids=[MY_GUILD_ID], description="Sends the list of commands, most are self-explanatory")
 async def help(ctx):
     await ctx.respond("Options:\n"
+                      "`time` - tell the time in various timezones\n"
                       "`help` - show this message\n" 
-                      "`github` - sends the link to my repository\n"
-                      "`Hungary` - tells the time in Hungary\n"
-                      "`Persistent` - sends an update on Hungary's time every hour,"
-                      " like a clock tower\n"
-                      "`Philippines` - tells the time in the Philippines\n"
-                      "`Alaska` - tells the time in Alaska\n"
-                      "`Mountain Time` - tells the US Mountain Time\n"
-                      "`Central Time` - tells the US Central Time\n"
-                      "`Eastern Time` - tells the US Eastern Standard Time\n"
-                      "`Pacific Time` - tell the US Pacific Time\n"
-                      "`American Samoa` - tell the time in the American Samoa\n"
-                      "`United Kingdom` - tell the time in the United Kingdom\n")
+                      "`github` - sends the link to my repository\n")
 
 bot.run(os.getenv('TOKEN'))
